bcee7870d73723c752ee18bd8669e3052a1f1ec4
八木 達雄yagi.tatsuo@jp.panasonic.com <3911915@pcc-ad.pcc.mei.co.jp>
2019-07-12 14:53:40 +0900

    ADC0_1_4_HSI commit

---
+++ MDK-ARM/ADC0_1_4_HSI/startup_stm32l072xx.d
@@ -0,0 +1,1 @@
+adc0_1_4_hsi\startup_stm32l072xx.o: startup_stm32l072xx.s
---
+++ MDK-ARM/RTE/_ADC0_1_4_HSI/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ADC0_1_4_HSI' 
+ * Target:  'ADC0_1_4_HSI' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
